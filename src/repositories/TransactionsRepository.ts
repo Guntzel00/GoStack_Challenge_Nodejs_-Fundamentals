f516773bd661ddf0acbb978254f2a69b2cb569ed
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2020-04-29 00:06:26 -0700

    Everything working, except by the post route

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,28 +1,61 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+  totalTransactions: number;
+}
+
+interface BankStatement {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  public total: number;
+
+  public income: number;
+
+  public outcome: number;
+
+  public totalTransactions: number;
+
   constructor() {
     this.transactions = [];
+    this.total = 0;
+    this.income = 0;
+    this.outcome = 0;
+    this.totalTransactions = 0;
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionDTO[] {
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      total: this.total,
+      income: this.income,
+      outcome: this.outcome,
+      totalTransactions: this.totalTransactions,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
